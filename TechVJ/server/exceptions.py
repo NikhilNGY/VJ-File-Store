1a8ce26a755662e24b73e83c667e5e338533cb69
NIKHIL <65222469+NikhilNGY@users.noreply.github.com>
2025-08-30 00:53:52 +0530

    Update exceptions.py

--- TechVJ/server/exceptions.py
+++ TechVJ/server/exceptions.py
@@ -1,6 +1,22 @@
+# TechVJ/server/exceptions.py
 
-class InvalidHash(Exception):
-    message = "Invalid hash"
+class TechVJException(Exception):
+    """Base exception for TechVJ server errors."""
+    default_message = "An unexpected error occurred"
 
-class FIleNotFound(Exception):
-    message = "File not found"
+    def __init__(self, message: str = None):
+        super().__init__(message or self.default_message)
+        self.message = message or self.default_message
+
+    def __str__(self):
+        return self.message
+
+
+class InvalidHash(TechVJException):
+    """Raised when a provided hash is invalid."""
+    default_message = "Invalid hash"
+
+
+class FileNotFound(TechVJException):
+    """Raised when a requested file is not found."""
+    default_message = "File not found"
