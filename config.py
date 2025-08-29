ef4e5b2cd1ce66d217cd7308cb1f30640313bcce
NIKHIL <65222469+NikhilNGY@users.noreply.github.com>
2025-08-29 23:50:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,89 +1,96 @@
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# -------------------------------
+# Credit Tg - @VJ_Botz
+# YouTube: https://youtube.com/@Tech_VJ
+# Telegram Support: @KingVJ01
+# -------------------------------
 
-
-import re
 import os
-from os import environ
-from Script import script
+import re
 
-id_pattern = re.compile(r'^.\d+$')
-def is_enabled(value, default):
+# -------------------------------
+# Helper Functions
+# -------------------------------
+def is_enabled(value: str, default: bool = True) -> bool:
     if value.lower() in ["true", "yes", "1", "enable", "y"]:
         return True
     elif value.lower() in ["false", "no", "0", "disable", "n"]:
         return False
     else:
         return default
-      
+
+# -------------------------------
+# Regex Patterns
+# -------------------------------
+id_pattern = re.compile(r'^\d+$')
+
+# -------------------------------
 # Bot Information
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
+# -------------------------------
+API_ID = int(os.environ.get("API_ID", 0))
+API_HASH = os.environ.get("API_HASH", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+BOT_USERNAME = os.environ.get("BOT_USERNAME", "")  # without @
+PORT = int(os.environ.get("PORT", 8080))
 
-PICS = (environ.get('PICS', 'https://graph.org/file/ce1723991756e48c35aa1.jpg')).split() # Bot Start Picture
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
-BOT_USERNAME = environ.get("BOT_USERNAME", "") # without @
-PORT = environ.get("PORT", "8080")
+PICS = os.environ.get('PICS', 'https://graph.org/file/ce1723991756e48c35aa1.jpg').split()
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMINS', '').split()]
 
-# Clone Info :-
-CLONE_MODE = bool(environ.get('CLONE_MODE', False)) # Set True or False
+# -------------------------------
+# Clone Mode
+# -------------------------------
+CLONE_MODE = is_enabled(os.environ.get('CLONE_MODE', "False"))
+CLONE_DB_URI = os.environ.get("CLONE_DB_URI", "")
+CDB_NAME = os.environ.get("CDB_NAME", "clonetechvj")
 
-# If Clone Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
-CLONE_DB_URI = environ.get("CLONE_DB_URI", "")
-CDB_NAME = environ.get("CDB_NAME", "clonetechvj")
-
+# -------------------------------
 # Database Information
-DB_URI = environ.get("DB_URI", "")
-DB_NAME = environ.get("DB_NAME", "techvjbotz")
+# -------------------------------
+DB_URI = os.environ.get("DB_URI", "")
+DB_NAME = os.environ.get("DB_NAME", "techvjbotz")
 
+# -------------------------------
 # Auto Delete Information
-AUTO_DELETE_MODE = bool(environ.get('AUTO_DELETE_MODE', True)) # Set True or False
+# -------------------------------
+AUTO_DELETE_MODE = is_enabled(os.environ.get('AUTO_DELETE_MODE', "True"))
+AUTO_DELETE = int(os.environ.get("AUTO_DELETE", 30))  # in minutes
+AUTO_DELETE_TIME = int(os.environ.get("AUTO_DELETE_TIME", 1800))  # in seconds
 
-# If Auto Delete Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
-AUTO_DELETE = int(environ.get("AUTO_DELETE", "30")) # Time in Minutes
-AUTO_DELETE_TIME = int(environ.get("AUTO_DELETE_TIME", "1800")) # Time in Seconds
+# -------------------------------
+# Channel / Logging Information
+# -------------------------------
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", 0))
 
-# Channel Information
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", ""))
-
+# -------------------------------
 # File Caption Information
-CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", f"{script.CAPTION}")
-BATCH_FILE_CAPTION = environ.get("BATCH_FILE_CAPTION", CUSTOM_FILE_CAPTION)
+# -------------------------------
+CUSTOM_FILE_CAPTION = os.environ.get("CUSTOM_FILE_CAPTION", "Uploaded by @Tech_VJ")
+BATCH_FILE_CAPTION = os.environ.get("BATCH_FILE_CAPTION", CUSTOM_FILE_CAPTION)
+PUBLIC_FILE_STORE = is_enabled(os.environ.get('PUBLIC_FILE_STORE', "True"))
 
-# Enable - True or Disable - False
-PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "True")), True)
+# -------------------------------
+# Verification Settings
+# -------------------------------
+VERIFY_MODE = is_enabled(os.environ.get('VERIFY_MODE', "False"))
+SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "")
+SHORTLINK_API = os.environ.get("SHORTLINK_API", "")
+VERIFY_TUTORIAL = os.environ.get("VERIFY_TUTORIAL", "")
 
-# Verify Info :-
-VERIFY_MODE = bool(environ.get('VERIFY_MODE', False)) # Set True or False
+# -------------------------------
+# Website Info
+# -------------------------------
+WEBSITE_URL_MODE = is_enabled(os.environ.get('WEBSITE_URL_MODE', "False"))
+WEBSITE_URL = os.environ.get("WEBSITE_URL", "")
 
-# If Verify Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
-SHORTLINK_URL = environ.get("SHORTLINK_URL", "") # shortlink domain without https://
-SHORTLINK_API = environ.get("SHORTLINK_API", "") # shortlink api
-VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "") # how to open link 
+# -------------------------------
+# Stream / Multi-Client Settings
+# -------------------------------
+STREAM_MODE = is_enabled(os.environ.get('STREAM_MODE', "True"))
+MULTI_CLIENT = is_enabled(os.environ.get('MULTI_CLIENT', "False"))
+SLEEP_THRESHOLD = int(os.environ.get('SLEEP_THRESHOLD', 60))
+PING_INTERVAL = int(os.environ.get("PING_INTERVAL", 1200))  # in seconds
 
-# Website Info:
-WEBSITE_URL_MODE = bool(environ.get('WEBSITE_URL_MODE', False)) # Set True or False
-
-# If Website Url Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
-WEBSITE_URL = environ.get("WEBSITE_URL", "") # For More Information Check Video On Yt - @Tech_VJ
-
-# File Stream Config
-STREAM_MODE = bool(environ.get('STREAM_MODE', True)) # Set True or False
-
-# If Stream Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
-MULTI_CLIENT = False
-SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
-PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-if 'DYNO' in environ:
-    ON_HEROKU = True
-else:
-    ON_HEROKU = False
-URL = environ.get("URL", "https://testofvjfilter-1fa60b1b8498.herokuapp.com/")
-
-
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-    
+# -------------------------------
+# Heroku / Deployment
+# -------------------------------
+ON_HEROKU = 'DYNO' in os.environ
+URL = os.environ.get("URL", "https://your-app.herokuapp.com/")
