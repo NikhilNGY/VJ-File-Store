dc67a3571149a874dcec17ff7d41b4f58e9cb227
NIKHIL <65222469+NikhilNGY@users.noreply.github.com>
2025-08-30 12:52:15 +0530

    Update start.py

--- TechVJ/start.py
+++ TechVJ/start.py
@@ -7,13 +7,20 @@ from pyrogram.enums import ParseMode
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.errors import FloodWait, UserIsBlocked, InputUserDeactivated
 
-from bot import Bot
-
-bot_instance = Bot()
-
 from config import ADMINS, FORCE_MSG, START_MSG, CUSTOM_CAPTION, DISABLE_CHANNEL_BUTTON, PROTECT_CONTENT
 from helper_func import subscribed, encode, decode, get_messages
 from database.database import add_user, del_user, full_userbase, present_user
+
+# Script.py
+
+# Option 1: as a class
+class script:
+    def __init__(self):
+        print("Script class initialized")
+
+# Option 2: as a function
+# def script():
+#     print("Script function called")
 
 # ======================== Constants ======================== #
 
