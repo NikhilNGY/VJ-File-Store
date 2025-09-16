92abbf1d083530b11c901e8edc0ad166f968887c
Finding.. <65222469+NikhilNGY@users.noreply.github.com>
2025-09-16 15:27:40 +0530

    Update config_parser.py

--- TechVJ/utils/config_parser.py
+++ TechVJ/utils/config_parser.py
@@ -3,17 +3,26 @@ from typing import Dict, Optional
 
 
 class TokenParser:
+    """
+    A class to parse MULTI_TOKEN variables from environment variables.
+    """
+
     def __init__(self, config_file: Optional[str] = None):
-        self.tokens = {}
+        self.tokens: Dict[int, str] = {}
         self.config_file = config_file
 
     def parse_from_env(self) -> Dict[int, str]:
-        self.tokens = dict(
-            (c + 1, t)
-            for c, (_, t) in enumerate(
-                filter(
-                    lambda n: n[0].startswith("MULTI_TOKEN"), sorted(environ.items())
-                )
+        """
+        Parse all environment variables that start with MULTI_TOKEN and
+        return them as a dictionary where keys are incremental integers starting from 1.
+
+        Returns:
+            Dict[int, str]: A dictionary of tokens.
+        """
+        self.tokens = {
+            index + 1: value
+            for index, (key, value) in enumerate(
+                filter(lambda item: item[0].startswith("MULTI_TOKEN"), sorted(environ.items()))
             )
-        )
+        }
         return self.tokens
